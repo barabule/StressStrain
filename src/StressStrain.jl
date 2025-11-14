69e5be0fb42d790b3c9d9a8452fe7c5a8c15f5e6
barabule <werner.zanzinger@gmail.com>
2025-11-14 09:57:55 +0200

    some exports

--- src/StressStrain.jl
+++ src/StressStrain.jl
@@ -11,5 +11,10 @@ include("hardening_laws.jl")
 include("data_gui.jl")
 include("gui.jl")
 
+export engineering_to_true, true_to_engineering
+export get_modulus
+export get_hardening_portion
+export toein_compensate
+export read_stress_strain_data
 
 end # module StressStrain
